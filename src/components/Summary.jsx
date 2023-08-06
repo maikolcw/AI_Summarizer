15abb62f3617f2219cd67755f816c3b9807d0815
maikolcw <whysoraccoony@gmail.com>
2023-08-05 20:23:14 -0700

    added input history, summary display, local storage for persistant data

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -7,8 +7,21 @@ const Summary = () => {
         url: '',
         summary: '',
     });
+    // We use this state to keep track of all articles we searched for
+    const [allArticles, setAllArticles] = useState([]);
 
+    // Lazy load
     const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+    useEffect(() => {
+        const articlesFromLocalStorage = JSON.parse(
+            localStorage.getItem("articles")
+        );
+
+        if (articlesFromLocalStorage) {
+            setAllArticles(articlesFromLocalStorage);
+        }
+    }, []);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -16,8 +29,13 @@ const Summary = () => {
         const { data } = await getSummary({ articleUrl: article.url });
         if (data?.summary) {
             const newArticle = { ...article, summary: data.summary };
+            const updatedAllArticles = [newArticle, ...allArticles];
 
             setArticle(newArticle);
+            // Update all articles state
+            setAllArticles(updatedAllArticles);
+            // When we get summary we set key 'articles' with value of all articles
+            localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
         }
     };
 
@@ -54,6 +72,56 @@ const Summary = () => {
                         />
                     </button>
                 </form>
+
+                {/* Summary history section */}
+                <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+                    {allArticles.reverse().map((item, index) => (
+                        <div
+                            key={`link-${index}`}
+                            onClick={() => setArticle(item)}
+                            className='link_card'
+                        >
+                            <div className='copy_btn'>
+                                <img
+                                    src={copy}
+                                    alt="copy_icon"
+                                    className='w-[40%] h-[40%] object-contain'
+                                />
+                            </div>
+                            <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+                                {item.url}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            {/* Article summary */}
+            <div className='my-10 max-w-full flex justify-center items-center'>
+                {isFetching ? (
+                    <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+                ) : error ? (
+                    <p className='font-inter font-bold text-black text-center'>
+                        Something went wrong...
+                        <br />
+                        <span className='font-satoshi font-normal text-gray-700'>
+                            {error?.data?.error}
+                        </span>
+                    </p>
+                ) : (
+                    article.summary && (
+                        <div className='flex flex-col gap-3'>
+                            <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                                Article <span className='blue_gradient'>Summary</span>
+                            </h2>
+                            <div className='summary_box'>
+                                <p className='font-inter font-medium text-sm text-gray-700'>
+                                    {article.summary}
+                                </p>
+                            </div>
+                        </div>
+                    )
+                )}
             </div>
         </section>
     );
